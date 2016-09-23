eb1caf2ed4722fe19e7ae50e4ea77e29c08ee21c
sandeepmaity09 <sandeepmaity09@gmail.com>
2016-09-23 16:40:30 +0530

    Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+class HelloWorld{
+public static void main(String ...k){
+System.out.println("Hello World!");
+System.out.println("Hello I'm Sandeep!");
+System.out.println("Welcome to my first program");
+}
+}
