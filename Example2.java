9bb8a30cb13e46918effed78ba402c54c3836bf5
sandeepmaity09 <sandeepmaity09@gmail.com>
2016-09-23 17:44:31 +0530

    Programs 23-09-16

---
+++ Example2.java
@@ -0,0 +1,9 @@
+class Example2 {
+public static void main(String[] args) {
+int num;
+num=100;
+System.out.println("This is a num."+num);
+num=num*2;
+System.out.println("The value on new num is."+num);
+}
+}
